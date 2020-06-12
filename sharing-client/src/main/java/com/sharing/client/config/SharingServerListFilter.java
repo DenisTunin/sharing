25b90f1c5c71fe68b2f5acc5357c5acd8ae81507
denistyunin <deni.tyunin@gmail.com>
2020-06-12 20:17:50 +0300

    Modify SharingServerListFilter

--- sharing-client/src/main/java/com/sharing/client/config/SharingServerListFilter.java
+++ sharing-client/src/main/java/com/sharing/client/config/SharingServerListFilter.java
@@ -4,10 +4,11 @@ import com.netflix.loadbalancer.Server;
 import com.netflix.loadbalancer.ServerListFilter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SharingServerListFilter<T extends Server> implements ServerListFilter <T>{
     @Override
     public List<T> getFilteredListOfServers(List<T> servers) {
-        return servers;
+        return servers.stream().filter(Server::isAlive).collect(Collectors.toList());
     }
 }
